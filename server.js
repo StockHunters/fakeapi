01e91e018bdbcaf8f4c3e9c14ba3f7b907097eba
faryd <mirandafaryd@gmail.com>
2025-05-10 19:29:14 -0500

    🐛 Fix: error in router

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const jsonServer = require('json-server');
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
+const jsonServer = require("json-server");
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
-const router = jsonServer.router({});
 
 app.use(cors());
 
-const clients = JSON.parse(fs.readFileSync('./db/clients.json'));
-const products = JSON.parse(fs.readFileSync('./db/products.json'));
-const sales = JSON.parse(fs.readFileSync('./db/sales.json'));
+const clients = JSON.parse(fs.readFileSync("./db/clients.json"));
+const products = JSON.parse(fs.readFileSync("./db/products.json"));
+const sales = JSON.parse(fs.readFileSync("./db/sales.json"));
 
-router.db.set('clients', clients);
-router.db.set('products', products);
-router.db.set('sales', sales);
+const dbData = {
+  clients,
+  products,
+  sales,
+};
 
+const router = jsonServer.router(dbData);
 
 app.use(jsonServer.defaults());
-app.use('/api', router);
+
+app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
